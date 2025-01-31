e865d2af00bf9e64eb8d52417661e241e5e98eb1
KamleshThakurOC <166357466+Kamlesh-Thakur-10@users.noreply.github.com>
2025-01-31 09:21:46 +0545

    UI revamp

--- FileWatcherConfig/Form1.Designer.cs
+++ FileWatcherConfig/Form1.Designer.cs
@@ -28,9 +28,6 @@
         /// </summary>
         private void InitializeComponent()
         {
-            txtFolderPath = new TextBox();
-            folderToWatchLabel = new Label();
-            btnBrowse = new Button();
             label1 = new Label();
             txtConnectionString = new TextBox();
             label2 = new Label();
@@ -45,92 +42,71 @@
             StartServiceButton = new Button();
             StopServiceButton = new Button();
             UninstallServiceButton = new Button();
+            groupBox1 = new GroupBox();
+            groupBox2 = new GroupBox();
+            txtFolderPath = new TextBox();
+            folderToWatchLabel = new Label();
+            btnBrowse = new Button();
+            groupBox1.SuspendLayout();
+            groupBox2.SuspendLayout();
             SuspendLayout();
-            // 
-            // txtFolderPath
-            // 
-            txtFolderPath.Location = new Point(204, 66);
-            txtFolderPath.Name = "txtFolderPath";
-            txtFolderPath.Size = new Size(569, 26);
-            txtFolderPath.TabIndex = 0;
-            // 
-            // folderToWatchLabel
-            // 
-            folderToWatchLabel.AutoSize = true;
-            folderToWatchLabel.Font = new Font("Aptos", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            folderToWatchLabel.Location = new Point(78, 70);
-            folderToWatchLabel.Name = "folderToWatchLabel";
-            folderToWatchLabel.Size = new Size(120, 19);
-            folderToWatchLabel.TabIndex = 1;
-            folderToWatchLabel.Text = "Folder to Watch:";
-            // 
-            // btnBrowse
-            // 
-            btnBrowse.Font = new Font("Aptos", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            btnBrowse.Location = new Point(781, 65);
-            btnBrowse.Name = "btnBrowse";
-            btnBrowse.Size = new Size(106, 28);
-            btnBrowse.TabIndex = 2;
-            btnBrowse.Text = "Browse";
-            btnBrowse.UseVisualStyleBackColor = true;
-            btnBrowse.Click += btnBrowse_Click;
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(15, 108);
+            label1.Location = new Point(11, 43);
             label1.Name = "label1";
-            label1.Size = new Size(183, 19);
+            label1.Size = new Size(190, 19);
             label1.TabIndex = 3;
-            label1.Text = "Azure Connection String: ";
+            label1.Text = "Storage Connection String";
             // 
             // txtConnectionString
             // 
-            txtConnectionString.Location = new Point(204, 105);
+            txtConnectionString.Location = new Point(207, 40);
             txtConnectionString.Name = "txtConnectionString";
             txtConnectionString.PasswordChar = '*';
-            txtConnectionString.Size = new Size(569, 26);
+            txtConnectionString.Size = new Size(542, 26);
             txtConnectionString.TabIndex = 4;
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(73, 149);
+            label2.Location = new Point(80, 75);
             label2.Name = "label2";
-            label2.Size = new Size(125, 19);
+            label2.Size = new Size(121, 19);
             label2.TabIndex = 7;
-            label2.Text = "Container Name:";
+            label2.Text = "Container Name";
             // 
             // txtContainerName
             // 
-            txtContainerName.Location = new Point(204, 146);
+            txtContainerName.Location = new Point(207, 72);
             txtContainerName.Name = "txtContainerName";
             txtContainerName.Size = new Size(290, 26);
             txtContainerName.TabIndex = 8;
             // 
             // txtLogFilePath
             // 
-            txtLogFilePath.Location = new Point(204, 187);
+            txtLogFilePath.Location = new Point(207, 72);
             txtLogFilePath.Name = "txtLogFilePath";
-            txtLogFilePath.Size = new Size(569, 26);
+            txtLogFilePath.Size = new Size(424, 26);
             txtLogFilePath.TabIndex = 10;
             txtLogFilePath.Click += txtLogFilePath_Click;
             // 
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(99, 190);
+            label3.Location = new Point(106, 75);
             label3.Name = "label3";
-            label3.Size = new Size(99, 19);
+            label3.Size = new Size(95, 19);
             label3.TabIndex = 9;
-            label3.Text = "Log File Path:";
+            label3.Text = "Log File Path";
             // 
             // btnLocationBrowse
             // 
             btnLocationBrowse.Font = new Font("Aptos", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            btnLocationBrowse.Location = new Point(781, 230);
+            btnLocationBrowse.Location = new Point(637, 104);
             btnLocationBrowse.Name = "btnLocationBrowse";
-            btnLocationBrowse.Size = new Size(106, 28);
+            btnLocationBrowse.Size = new Size(112, 28);
             btnLocationBrowse.TabIndex = 15;
             btnLocationBrowse.Text = "Browse";
             btnLocationBrowse.UseVisualStyleBackColor = true;
@@ -140,7 +116,7 @@
             // 
             label4.AutoSize = true;
             label4.Font = new Font("Aptos", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label4.Location = new Point(34, 235);
+            label4.Location = new Point(37, 108);
             label4.Name = "label4";
             label4.Size = new Size(164, 19);
             label4.TabIndex = 14;
@@ -148,16 +124,16 @@
             // 
             // txtServicePath
             // 
-            txtServicePath.Location = new Point(204, 231);
+            txtServicePath.Location = new Point(207, 104);
             txtServicePath.Name = "txtServicePath";
-            txtServicePath.Size = new Size(569, 26);
+            txtServicePath.Size = new Size(424, 26);
             txtServicePath.TabIndex = 13;
             // 
             // btnSaveConfig
             // 
             btnSaveConfig.BackColor = Color.LightGreen;
             btnSaveConfig.ForeColor = Color.DarkGreen;
-            btnSaveConfig.Location = new Point(643, 339);
+            btnSaveConfig.Location = new Point(704, 358);
             btnSaveConfig.Name = "btnSaveConfig";
             btnSaveConfig.Size = new Size(130, 39);
             btnSaveConfig.TabIndex = 16;
@@ -169,7 +145,7 @@
             // 
             InstallServeButton.BackColor = Color.LightGreen;
             InstallServeButton.ForeColor = Color.DarkGreen;
-            InstallServeButton.Location = new Point(507, 339);
+            InstallServeButton.Location = new Point(704, 403);
             InstallServeButton.Name = "InstallServeButton";
             InstallServeButton.Size = new Size(130, 39);
             InstallServeButton.TabIndex = 17;
@@ -181,7 +157,7 @@
             // 
             StartServiceButton.BackColor = Color.LightGreen;
             StartServiceButton.ForeColor = Color.DarkGreen;
-            StartServiceButton.Location = new Point(371, 339);
+            StartServiceButton.Location = new Point(704, 448);
             StartServiceButton.Name = "StartServiceButton";
             StartServiceButton.Size = new Size(130, 39);
             StartServiceButton.TabIndex = 18;
@@ -193,7 +169,7 @@
             // 
             StopServiceButton.BackColor = Color.LightCoral;
             StopServiceButton.ForeColor = Color.DarkRed;
-            StopServiceButton.Location = new Point(235, 339);
+            StopServiceButton.Location = new Point(568, 448);
             StopServiceButton.Name = "StopServiceButton";
             StopServiceButton.Size = new Size(130, 39);
             StopServiceButton.TabIndex = 19;
@@ -205,7 +181,7 @@
             // 
             UninstallServiceButton.BackColor = Color.LightCoral;
             UninstallServiceButton.ForeColor = Color.DarkRed;
-            UninstallServiceButton.Location = new Point(99, 339);
+            UninstallServiceButton.Location = new Point(568, 403);
             UninstallServiceButton.Name = "UninstallServiceButton";
             UninstallServiceButton.Size = new Size(130, 39);
             UninstallServiceButton.TabIndex = 20;
@@ -213,40 +189,86 @@
             UninstallServiceButton.UseVisualStyleBackColor = false;
             UninstallServiceButton.Click += UninstallServiceButton_Click;
             // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(txtConnectionString);
+            groupBox1.Controls.Add(label1);
+            groupBox1.Controls.Add(label2);
+            groupBox1.Controls.Add(txtContainerName);
+            groupBox1.Location = new Point(67, 32);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(772, 123);
+            groupBox1.TabIndex = 21;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Azure Configuration";
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(txtFolderPath);
+            groupBox2.Controls.Add(folderToWatchLabel);
+            groupBox2.Controls.Add(btnBrowse);
+            groupBox2.Controls.Add(txtLogFilePath);
+            groupBox2.Controls.Add(label3);
+            groupBox2.Controls.Add(txtServicePath);
+            groupBox2.Controls.Add(label4);
+            groupBox2.Controls.Add(btnLocationBrowse);
+            groupBox2.Location = new Point(67, 172);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(772, 158);
+            groupBox2.TabIndex = 22;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "File Watcher Service Configuration";
+            // 
+            // txtFolderPath
+            // 
+            txtFolderPath.Location = new Point(207, 40);
+            txtFolderPath.Name = "txtFolderPath";
+            txtFolderPath.Size = new Size(424, 26);
+            txtFolderPath.TabIndex = 7;
+            // 
+            // folderToWatchLabel
+            // 
+            folderToWatchLabel.AutoSize = true;
+            folderToWatchLabel.Font = new Font("Aptos", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            folderToWatchLabel.Location = new Point(85, 43);
+            folderToWatchLabel.Name = "folderToWatchLabel";
+            folderToWatchLabel.Size = new Size(116, 19);
+            folderToWatchLabel.TabIndex = 8;
+            folderToWatchLabel.Text = "Folder to Watch";
+            // 
+            // btnBrowse
+            // 
+            btnBrowse.Font = new Font("Aptos", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnBrowse.Location = new Point(637, 38);
+            btnBrowse.Name = "btnBrowse";
+            btnBrowse.Size = new Size(112, 28);
+            btnBrowse.TabIndex = 9;
+            btnBrowse.Text = "Browse";
+            btnBrowse.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(9F, 19F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(900, 407);
+            ClientSize = new Size(900, 523);
+            Controls.Add(groupBox2);
+            Controls.Add(groupBox1);
             Controls.Add(UninstallServiceButton);
             Controls.Add(StopServiceButton);
             Controls.Add(StartServiceButton);
             Controls.Add(InstallServeButton);
             Controls.Add(btnSaveConfig);
-            Controls.Add(btnLocationBrowse);
-            Controls.Add(label4);
-            Controls.Add(txtServicePath);
-            Controls.Add(txtLogFilePath);
-            Controls.Add(label3);
-            Controls.Add(txtContainerName);
-            Controls.Add(label2);
-            Controls.Add(txtConnectionString);
-            Controls.Add(label1);
-            Controls.Add(btnBrowse);
-            Controls.Add(folderToWatchLabel);
-            Controls.Add(txtFolderPath);
             Font = new Font("Aptos", 9F, FontStyle.Bold);
             Name = "Form1";
             Text = "File Watcher Config App";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            groupBox2.ResumeLayout(false);
+            groupBox2.PerformLayout();
             ResumeLayout(false);
-            PerformLayout();
         }
 
         #endregion
-
-        private TextBox txtFolderPath;
-        private Label folderToWatchLabel;
-        private Button btnBrowse;
         private Label label1;
         private TextBox txtConnectionString;
         private Label label2;
@@ -261,5 +283,10 @@
         private Button StartServiceButton;
         private Button StopServiceButton;
         private Button UninstallServiceButton;
+        private GroupBox groupBox1;
+        private GroupBox groupBox2;
+        private TextBox txtFolderPath;
+        private Label folderToWatchLabel;
+        private Button btnBrowse;
     }
 }
